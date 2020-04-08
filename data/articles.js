3a45fff315b46fbab16567f3e72e4e5d7904a9e4
Wes Cutting <wesleycutting@gmail.com>
2020-04-08 14:50:08 -0600

    Implemented CREATE Article in Service and DAL

--- data/articles.js
+++ data/articles.js
@@ -11,6 +11,28 @@ const colName = 'articles';
 // Database Settings
 const settings = { useUnifiedTopology: true }
 
+// Article Validator Function (Title & Link required, Link needs proper formatting)
+const invalidArticle = (article) => {
+    let result;
+    if(!article.title){
+        result = 'Articles require a Title';
+    } else if (!article.link){
+        result = 'Articles require a Link';
+    } else if (!validURL(article.link)) {
+        result = 'Link not valid URL';
+    }
+    return result;
+}
+const validURL = (str) => {
+    const pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+        '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+    return pattern.test(str);
+}
+
 const getArticles = () => {
     const iou = new Promise((resolve, reject) => {
         // Use connect method to connect to the server
@@ -39,6 +61,42 @@ const getArticles = () => {
     return iou;
 }
 
+const addArticle = (articles) => {
+    const iou = new Promise((resolve, reject) => {
+        if (!Array.isArray(articles)) {
+            reject({ msg: 'Need to send an Array of Articles' });
+        } else {
+            const invalidArticles = articles.filter((article) => {
+                const check = invalidArticle(article);
+                if(check){
+                    article.invalid = check;
+                }
+                return article.invalid;
+            });
+            if(invalidArticles.length > 0){
+                reject({
+                    msg: 'Some Articles were invalid',
+                    data: invalidArticles
+                })
+            }else {
+                MongoClient.connect(url, settings, async function (err, client) {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        console.log("Connected successfully to server to POST Articles");
+                        const db = client.db(dbName);
+                        const collection = db.collection(colName);
+                        const results = await collection.insertMany(articles);
+                        resolve(results.ops);
+                    }
+                })
+            }
+        }  
+    });
+    return iou;
+}
+
 module.exports = {
-    getArticles
+    getArticles, 
+    addArticle
 }
--- routes/api/articles.js
+++ routes/api/articles.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getArticles } = require('../../data/articles')
+const { 
+    getArticles,
+    addArticle 
+} = require('../../data/articles')
 
 /* GET articles listing. */
 router.get('/', async function (req, res, next) {
@@ -9,7 +12,23 @@ router.get('/', async function (req, res, next) {
         res.send(data);
     } catch(err){
         console.log(err);
-        res.send(500, 'Internal Server Issue, check logs');
+        res.status(500).send('Internal Server Issue, check logs');
+    }
+});
+
+/* POST article Creation */
+router.post('/', async function(req, res, next) {
+    try{
+        // TODO Handling Request Data and Create Function Call
+        const data = await addArticle(req.body);
+        res.send(data);
+    } catch(err){
+        if(err.msg){
+            res.status(400).send(err);
+        }else {
+            console.log(err);
+            res.status(500).send('Internal Server Issue, check logs');
+        }
     }
 });
 
