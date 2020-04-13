d2a9d223f96a8a2077cb2202626ca2bbb55c6b96
Wes Cutting <wesleycutting@gmail.com>
2020-04-13 03:19:38 -0600

    Removed UPDATE & DELETE functionality for Deployment

--- public/javascripts/articles.js
+++ public/javascripts/articles.js
@@ -5,13 +5,6 @@ const author = document.getElementById('author');
 const topics = document.getElementById('topics');
 const desc = document.getElementById('desc');
 const message = document.getElementById('msg');
-const deleteButtons = document.getElementsByClassName('delete');
-const updateButtons = document.getElementsByClassName('edit');
-const deleteModal = document.getElementById("deleteModal");
-const updateModal = document.getElementById("updateModal");
-const closeDelete = document.getElementsByClassName("close")[0];
-const closeUpdate = document.getElementsByClassName("close")[1];
-const deleteBtn = document.getElementById('delete');
 
 const formHandler = (event) => {
     message.innerHTML = "";
@@ -34,18 +27,6 @@ const formHandler = (event) => {
                 break;
         }
     }
-}
-let deleteID;
-const openDeleteModal = (event) => {
-    deleteID = event.target.id;
-    deleteModal.style.display = "block";
-}
-
-let updateData;
-const openUpdateModal = (event) => {
-    updateData = event.target.value;
-    console.log(updateData);
-    updateModal.style.display = "block";
 }
 
 const addArticle = () => {
@@ -79,38 +60,4 @@ const addArticle = () => {
         })
         .catch(console.log);
         // .catch(error => console.log(error));
-}
-const updateArticle = () => {
-    
-}
-
-const deleteArticle = () => {
-    fetch(`/api/articles/${deleteID}`, {
-        method: 'DELETE'
-    }).then(() => window.location.replace('/articles'));
-}
-
-button.addEventListener('click', formHandler);
-for (let i = 0; i < deleteButtons.length; i++) {
-    deleteButtons[i].addEventListener('click', openDeleteModal);
-}
-for (let i = 0; i < updateButtons.length; i++) {
-    updateButtons[i].addEventListener('click', openUpdateModal);
-}
-deleteBtn.addEventListener('click', deleteArticle);
-closeDelete.onclick = function () {
-    deleteId = null;
-    deleteModal.style.display = "none";
-}
-closeUpdate.onclick = function () {
-    updateModal.style.display = "none";
-}
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == deleteModal) {
-        deleteId = null;
-        deleteModal.style.display = "none";
-    } else if (event.target == updateModal) {
-        updateModal.style.display = "none";
-    }
 }
